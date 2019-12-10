d708407d60302924e212b48af0f1649b219a6290
Md Ansari <MAN261@crb.apmoller.net>
2019-12-10 18:11:11 +0100

    First Proxy

---
+++ apiproxy/resources/jsc/JavaScript-1.js
@@ -0,0 +1,10 @@
+ var request_start_time = context.getVariable('client.received.start.timestamp');
+var target_start_time  = context.getVariable('target.sent.end.timestamp');
+var target_end_time    = context.getVariable('target.received.start.timestamp');
+var request_end_time   = context.getVariable('system.timestamp');
+var total_request_time = request_end_time-request_start_time;
+var total_target_time  = target_end_time-target_start_time;
+var total_proxy_time  = total_request_time-total_target_time;
+context.setVariable('log.latency.request.total', request_end_time - request_start_time);
+context.setVariable('log.latency.target', target_end_time - target_start_time);
+context.setVariable('log.latency.proxy', total_request_time - total_target_time );
